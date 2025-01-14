7407c5ac6351bcd013a14f72534cd1d8b4e94640
Soham Patil <sohamp1504@gmail.com>
2025-01-14 17:58:30 +0530

    Create Code.java

---
+++ Program 3/Code.java
@@ -0,0 +1,23 @@
+//Develope The Program Of Addition Of Two Number 
+
+import java.util.*;
+class Code{
+  public static void main (String [] args) {
+
+  int x ,y ;
+  double div;
+  Scanner n = new Scanner (System.in);
+  System.out.println("Enter The Two Number ");
+  x = n.nextInt();
+  y = n.nextInt();
+
+  double a = x ;
+  double b = y ;
+
+  div = a /b ;
+  System.out.println("Division : " + div);
+
+   n.close();
+
+ }
+}
