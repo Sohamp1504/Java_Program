936d36b65e9a97cbd522ac39cf98fe36927efdc1
Soham Patil <sohamp1504@gmail.com>
2025-01-14 19:52:52 +0530

    Create Code.java

---
+++ Program 5/Code.java
@@ -0,0 +1,31 @@
+
+//Develope The Program Of Complete Division Of Two Number 
+
+import java.util.*;
+class Code{
+  public static void main (String [] args) {
+
+  int x ,y , temp;
+
+  Scanner n = new Scanner (System.in);
+  System.out.println("Enter The Value Of X :  ");
+  x = n.nextInt();
+  System.out.println("Enter The Value Of Y :  ");
+  y = n.nextInt();
+
+  System.out.println(" Before Swapping ");
+  System.out.println(" The Value Of X :  " + x);
+  System.out.println(" The Value Of Y :  " + y);
+  
+   temp = x;
+   x = y;
+   y = temp;
+ 
+  System.out.println(" After Swapping ");
+  System.out.println(" The Value Of X :  " + x);
+  System.out.println(" The Value Of Y :  " + y);
+ 
+   n.close();
+
+ }
+}
