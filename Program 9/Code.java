faf80bcec7908d9ad1513c38ba4e1bd86d888fda
Soham Patil <sohamp1504@gmail.com>
2025-01-15 16:32:26 +0530

    Create Code.java

---
+++ Program 9/Code.java
@@ -0,0 +1,22 @@
+
+//Develope The Program Of Find Out Weather Number Is Positive Or Negative
+
+import java.util.*;
+class Code{
+  public static void main (String [] args) {
+
+  int x ;
+
+  Scanner n = new Scanner (System.in);
+  System.out.println("Enter The Number ");
+  x = n.nextInt();
+  
+   if (x>0)
+  System.out.println(x+" Is Number Is Positive Intergers  " );
+  else
+  System.out.println(x+" Is Number Is Negative Intergers  " );
+
+   n.close();
+
+ }
+}
