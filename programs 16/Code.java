bdd9dd14351bac2a0e185050e051dae81878ff90
Soham Patil <sohamp1504@gmail.com>
2025-01-17 17:21:56 +0530

    Create Code.java

---
+++ programs 16/Code.java
@@ -0,0 +1,24 @@
+
+//Develope The Program Of Find The Sum of Digit Of a Number
+
+import java.util.*;
+class Code{
+  public static void main (String [] args) {
+
+  int n, sum =0 , r;
+
+  Scanner in = new Scanner (System.in);
+  System.out.println("Enter The Number ");
+  n = in.nextInt();
+
+  while(n!=0){
+   r=n%10;
+   sum+=r;
+   n/=10;
+  }
+  System.out.println("Sum Of Digit  :  "+sum);
+
+   in.close();
+
+ }
+}
