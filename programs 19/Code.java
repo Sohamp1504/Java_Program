218a95a7b2b131a3c04fc3bc4587b1044c331624
Soham Patil <sohamp1504@gmail.com>
2025-01-19 18:27:29 +0530

    Create Code.java

---
+++ programs 19/Code.java
@@ -0,0 +1,33 @@
+
+//Develope The Program Ot Read The Number And Then Findout weather It is Palindrome Number Or Not
+// example 121, 323, 434
+
+import java.util.*;
+class Code{
+  public static void main (String [] args) {
+
+  int n , ori , rev =0, digit;
+
+  Scanner in = new Scanner (System.in);
+  System.out.println("Enter The Number ");
+  n = in.nextInt();
+
+  ori = n;
+
+  while(n>0)
+  {
+   digit = n%10;
+   rev = rev * 10 + digit;
+   n /= 10;
+   }
+   
+  if (ori==rev)
+      System.out.println(ori+" Is A Palindrome Number");
+  else
+      System.out.println(ori+" Is Not A Palindrome Number");
+ 
+
+   in.close();
+
+ }
+}
