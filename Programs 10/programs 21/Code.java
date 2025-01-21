79bc458b6e0ecac1000dba8c6ebab5b1480345fa
Soham Patil <sohamp1504@gmail.com>
2025-01-21 17:29:15 +0530

    Create Code.java

---
+++ Programs 10/programs 21/Code.java
@@ -0,0 +1,29 @@
+
+//Develope The Program Of Create The Array Of N Number  And Accept The Value From User And Print This Array Using The For Each loop
+
+import java.util.Scanner;
+
+class Code {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int num = in.nextInt();
+
+        int[] a = new int[num]; 
+
+        System.out.println("Enter the elements of the array:");
+        int index = 0; 
+        for (int x : a) { 
+            a[index] = in.nextInt();
+            index++;
+        }
+
+        System.out.println("The elements of the array are:");
+        for (int n : a) { 
+            System.out.println(n);
+        }
+
+        in.close();
+    }
+}
