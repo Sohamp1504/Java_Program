9c5b5e3ffe97f9b49ffc4a5a8771202013be2aa6
Soham Patil <sohamp1504@gmail.com>
2025-01-26 18:51:34 +0530

    Update Code.java

--- programs 30/Code.java
+++ programs 30/Code.java
@@ -1,1 +1,34 @@
 // Develope The Program To Find The maximum And Minimum Number From the Array
+
+// deveolpe The Program To Find The Sum And Average Of 5 Intergers
+
+import java.util.*;
+
+class Code {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] a = new int[5];
+        int sum = 0;
+        double avg;
+
+        System.out.println("Enter Five numbers:");
+        for (int i = 0; i < 5; i++) {
+            a[i] = in.nextInt();
+        }
+     int max = a[0] , min=a[0];
+
+     for (int i =1 ; i<a.length ; i++)
+     {
+        if (a[i] > max)
+             max= a[i];
+
+         if (a[i] < min)
+             min= a[i];
+      }
+
+  System.out.println("Maximum Number Of Array:"+max);
+  System.out.println("Minimum Number Of Array:"+min);
+        in.close();
+    }
+}
