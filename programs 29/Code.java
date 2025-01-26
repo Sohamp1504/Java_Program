28dfb3443097d028fcc35a0c4d903de1c25546e6
Soham Patil <sohamp1504@gmail.com>
2025-01-26 18:40:47 +0530

    Update Code.java

--- programs 29/Code.java
+++ programs 29/Code.java
@@ -1,1 +1,26 @@
 // deveolpe The Program To Find The Sum And Average Of 5 Intergers
+
+import java.util.*;
+
+class Code {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] a = new int[5];
+        int sum = 0;
+        double avg;
+
+        System.out.println("Enter Five numbers:");
+        for (int i = 0; i < 5; i++) {
+            a[i] = in.nextInt();
+            sum += a[i];
+        }
+     
+        avg = (double) sum / a.length;
+    
+        System.out.println("Sum of the numbers: " + sum);
+        System.out.println("Average of the numbers: " + avg);
+
+        in.close();
+    }
+}
